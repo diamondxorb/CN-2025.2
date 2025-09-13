37b83ef1a565908ce1aa15200f6776c078ef9028
Júlia <168246513+diamondxorb@users.noreply.github.com>
2025-09-13 10:55:42 -0300

    Add files via upload

---
+++ Lista 1/A.1 - Linear.f90
@@ -0,0 +1,21 @@
+program cncn
+    implicit none
+
+    integer :: n
+    real, dimension(0:100) :: x
+    real :: a, b=0
+
+    read(*,*) a
+
+    x(0) = 1.0
+
+    open(unit=127, file="saida.dat", status="unknown")
+
+    do n=0,99
+        x(n+1) = a*x(n) + b
+        write(127,*) x(n), x(n+1)
+    end do
+    
+    close(127)
+
+end program cncn
